f9773ae7141e17b4f5c2ec549e2546d97aec79a8
Amgad Malik <amgadmalik@yahoo.com>
2019-12-28 17:35:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @user_name = "#{params[:word1]}"+" #{params[:word2]}" +" #{params[:word3]}"+" #{params[:word4]}"+" #{params[:word5]}"
+    @user_name = "#{params[:word1]}"+" #{params[:word2]}" +" #{params[:word3]}"+" #{params[:word4]}"+" #{params[:word5]}."
     "#{@user_name}"
   end
   
